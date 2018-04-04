3a2085e91b8f124ddb3f11aa09604caddf8850fe
Rachel <rlmandelbaum@gmail.com>
2018-04-03 22:34:35 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+
+  for (let char of str){
+    chars[char] = chars[char] + 1 || 1;
+  }
+
+  console.log(chars)
+   let highestChar = Object.keys(chars).reduce((a, b) => { return chars[a] > chars[b] ? a : b} )
+   return highestChar
+   console.log(highestChar)
+ }
 
 module.exports = maxChar;
