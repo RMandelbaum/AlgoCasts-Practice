b94e135d1be797d82478a4894a9988b405f662ca
Rachel <rlmandelbaum@gmail.com>
2018-04-05 21:57:56 -0400

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //[1,2,3,4,5,6,7,8,9,10] divide it into chunks of 2
+  //[[1,2],[3,4],[5,6],[7,8],[9,10]]
+  const chunkArray = []
+  let index = 0;
+    while (array.length > index){
+      chunkArray.push(array.slice(index, index + size))
+      index += size
+}
+  return chunkArray
+
+}
 
 module.exports = chunk;
