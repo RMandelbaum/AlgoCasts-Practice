50994d3035cc158dcfec11931e8d4ca720a0b296
Rachel <rlmandelbaum@gmail.com>
2018-04-25 10:34:30 -0400

    recursive solution/fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,21 +9,20 @@
 //   fib(4) === 3
 
 function fib(n) {
-  //compute fib up to the nth element
-  // let's say n = 5
-  //we want to generate [0, 1, 1, 2, 3, 5]
-  //return 5
-  let sequence = [0, 1]
-  let counter = n
-  while(counter > 0){
-    let nextNumber = sequence[sequence.length-1] + sequence[sequence.length-1 -1]
-    sequence.push(nextNumber)
-    counter = counter - 1
+
+  if( n < 2){
+    return n;
   }
-  return sequence[n]
-  //sequence[-1] + sequence[-1 - 1] = nextNumber
-  //sequence.push(nextNumber)
-  //in fib, go the the index n of fibonacci array and return it
+
+  return fib(n-1) + fib(n-2)
+  // let sequence = [0, 1]
+  // let counter = 0
+  // while(counter < n){
+  //   let nextNumber = sequence[sequence.length - 1] + sequence[sequence.length - 2]
+  //   sequence.push(nextNumber)
+  //   counter++
+  // }
+  // return sequence[n]
 }
 
 
